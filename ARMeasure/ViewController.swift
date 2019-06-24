95589939315eaf154988d3904a090b4121f78c4c
justin-rose <jrose1887@gmail.com>
2019-06-24 12:07:42 -0700

    Add measure capabilities and 3D text in AR

--- ARMeasure/ViewController.swift
+++ ARMeasure/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  ARMeasure
-//
+//  An app to measure objects from 2 touch points in AR
 //  Created by Justin Rose on 6/22/19.
 //  Copyright © 2019 Justin Rose. All rights reserved.
 //
@@ -13,21 +13,17 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var dotNodes = [SCNNode]()
+    var previousTextNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,30 +42,73 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
         
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint) //we use .featurePoint because we want to detect a point on a detected surface closest to the touch point
+            if let hitTestResult = hitTestResults.first {
+                addDot(at: hitTestResult)
+            }
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func addDot(at location: ARHitTestResult) {
         
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(location.worldTransform.columns.3.x,
+                                      location.worldTransform.columns.3.y,
+                                      location.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+    }
+    
+    func calculate() {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        let xDistance = end.position.x - start.position.x
+        let yDistance = end.position.y - start.position.y
+        let zDistance = end.position.z - start.position.z
+        
+        //use Pythagorean theorem to find the distance between points in 3d
+        let distance = sqrtf(powf(xDistance, 2) + powf(yDistance, 2) + powf(zDistance, 2))
+        
+        displayText(distance, at: end.position)
+    }
+    
+    func displayText(_ distance: Float, at position: SCNVector3) {
+        
+        previousTextNode?.removeFromParentNode()
+        
+        let text = SCNText(string: String(distance * 3.281) + "ft", extrusionDepth: 1.0)
+        
+        text.firstMaterial?.diffuse.contents = UIColor.red //we can use the firstMaterial property instead of creating an SCNMaterial object first
+        
+        let node = SCNNode(geometry: text) //set the geometry when initializing the object rather than setting the geometry property later
+        
+        node.position = SCNVector3(position.x, position.y + 0.1, position.z - 5.0)
+        node.scale = SCNVector3(0.05, 0.05, 0.05) //the default text size is too big so we'll reduce the scale
+        previousTextNode = node
+        sceneView.scene.rootNode.addChildNode(node)
     }
 }
